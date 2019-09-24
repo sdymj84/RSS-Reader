e7db59fe43f0621b8d126ad0d4e45d36ab8e2c1c
Minjun Youn <minjun1.y@partner.samsung.com>
2019-09-24 17:49:40 -0500

    Add parse RSS feed and display

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require('express')
 const path = require('path')
 const members = require('./models/Members')
 const exphbs = require('express-handlebars')
+const fetch = require('node-fetch')
+const Parser = require('rss-parser')
+const parser = new Parser({
+  customFields: {
+    item: [
+      'media:content',
+      'media:group',
+    ]
+  }
+})
+
 
 const app = express()
 
@@ -14,17 +25,44 @@ app.set('view engine', 'handlebars');
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
+
 // Routes
-app.get('/', (req, res) => {
-  res.render('home')
+app.get('/', async (req, res) => {
+  // Get feed urls from database
+  let feedUrls = []
+  feedUrls = [
+    // 'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml',
+    // 'https://www.reddit.com/.rss',
+    'https://www.youtube.com/feeds/videos.xml?channel_id=UCoookXUzPciGrEZEXmh4Jjg',
+  ]
+
+  // Parse from feed
+  const feeds = []
+  await Promise.all(feedUrls.map(async feedUrl => {
+    try {
+      const feed = await parser.parseURL(feedUrl)
+      feed.items.forEach(item => {
+        console.log(item)
+
+        const media = item['media:content']
+        feeds.push({
+          title: item.title,
+          link: item.link,
+          pubDate: item.pubDate,
+          description: item.content,
+          media: media ? media['$'].url : "",
+        })
+      });
+    } catch (e) {
+      console.error('Error in fetching the website', e)
+    }
+  }))
+
+  console.log(feeds[0])
+  // Show result
+  res.render('home', { feeds: feeds })
 })
 
-app.get('/api/members', (req, res) => {
-  res.json(members)
-})
-app.get('/api/members/:id', (req, res) => {
-  res.json(req.params.id)
-})
 
 app.listen(PORT, () => {
   console.log(`Server Started on ${PORT}...`)
---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+$('.ui.dropdown')
+  .dropdown();
+
+function redirect(url) {
+  console.log('haha')
+  window.location.href = url
+}
