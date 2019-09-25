57235ad4069a521b9de5592ca42efc0ce7f818a7
sdymj84 <sdymj84@gmail.com>
2019-09-24 23:21:09 -0500

    Update customization for more feeds and Add read more

--- app.js
+++ app.js
@@ -3,26 +3,28 @@ const path = require('path')
 const members = require('./models/Members')
 const exphbs = require('express-handlebars')
 const fetch = require('node-fetch')
+const moment = require('moment')
 const Parser = require('rss-parser')
+const _ = require('lodash')
 const parser = new Parser({
   customFields: {
     item: [
       'media:content',
       'media:group',
+      'image',
     ]
   }
 })
 
 
 const app = express()
-
 const PORT = process.env.PORT || 5000
-
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-// Set static folder
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 
@@ -32,8 +34,8 @@ app.get('/', async (req, res) => {
   let feedUrls = []
   feedUrls = [
     // 'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml',
-    // 'https://www.reddit.com/.rss',
-    'https://www.youtube.com/feeds/videos.xml?channel_id=UCoookXUzPciGrEZEXmh4Jjg',
+    'http://www.espn.com/espn/rss/news',
+    // 'https://www.youtube.com/feeds/videos.xml?channel_id=UCoookXUzPciGrEZEXmh4Jjg',
   ]
 
   // Parse from feed
@@ -42,15 +44,31 @@ app.get('/', async (req, res) => {
     try {
       const feed = await parser.parseURL(feedUrl)
       feed.items.forEach(item => {
-        console.log(item)
+        let media = ""
+        let description = item.content
 
-        const media = item['media:content']
+        // Youtube
+        if (item['media:group']) {
+          media = item['media:group']['media:thumbnail'][0].$.url
+          description = item['media:group']['media:description'][0]
+        }
+        // News
+        else if (item['media:content']) {
+          media = item['media:content'].$.url
+        }
+        // ESPN
+        else if (item.image) {
+          media = item.image
+        }
+        else {
+          media = ""
+        }
         feeds.push({
           title: item.title,
           link: item.link,
-          pubDate: item.pubDate,
-          description: item.content,
-          media: media ? media['$'].url : "",
+          pubDate: moment(item.pubDate).format("MM/D/YYYY hh:mm"),
+          description,
+          media,
         })
       });
     } catch (e) {
@@ -58,7 +76,11 @@ app.get('/', async (req, res) => {
     }
   }))
 
-  console.log(feeds[0])
+  // const sort = req.query.sortby || ""
+  // console.log(sort)
+  // const sortedFeeds = _.sortBy(feeds, sort)
+  // console.log(sortedFeeds)
+
   // Show result
   res.render('home', { feeds: feeds })
 })
--- public/js/script.js
+++ public/js/script.js
@@ -5,3 +5,51 @@ function redirect(url) {
   console.log('haha')
   window.location.href = url
 }
+
+
+
+// Read more logics
+$(".article-description p").each(function () {
+  const lines = lineCounter($(this)[0])
+  if (lines < 3) {
+    $(this).next().hide()
+  } else {
+    $(this).addClass('block-with-text')
+  }
+})
+function lineCounter(el) {
+  var divHeight = el.offsetHeight
+  var lineHeight = parseInt(window.getComputedStyle(el).getPropertyValue('line-height'))
+  var lines = divHeight / lineHeight
+  return lines
+}
+$(".article-description a").on('click', function (e) {
+  if ($(this).text() === 'Read more...') {
+    $(this).html('Close')
+    $(this).prev().removeClass('block-with-text')
+  } else {
+    $(this).html('Read more...')
+    $(this).prev().addClass('block-with-text')
+  }
+})
+
+
+
+// Sort Dropdown change logics
+$(".dropdown").dropdown({
+  onChange: function (val) {
+    console.log(val)
+    $.get(`?sortby=${val}`)
+    $.ajax({
+      url: `?sortby=${val}`,
+      type: 'GET',
+      success: function (res) {
+        console.log(res)
+        // redirect('/')
+      },
+      error: function () {
+        console.log('error')
+      }
+    })
+  }
+})
